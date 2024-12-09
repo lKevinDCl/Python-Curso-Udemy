7cf2ad7313e8b8ddcb67aad22cc29db3da36c467
lKevinDCl <kevindiego2608@gmail.com>
2024-12-08 21:00:01 -0600

    Day 8: Lesson 4

---
+++ Día 8/archivo1.py
@@ -0,0 +1,11 @@
+"""
+Es un modulo de testeo
+"""
+
+
+def una_funcion():
+    numero1 = 500
+    print(numero1)
+
+
+una_funcion()
---
+++ Día 8/practica_pylint.py
@@ -0,0 +1,12 @@
+"""
+Función de práctica
+"""
+
+def sumar(numero1, numero2):
+    """
+    Suma dos números y devuelve el resultado.
+    """
+    return numero1 + numero2
+
+suma = sumar(5, 7)
+print(suma)
