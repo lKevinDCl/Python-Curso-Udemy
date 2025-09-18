773343a66510c7fcb4c6dcde35edecd4b81449be
lKevinDCl <kevindiego2608@gmail.com>
2025-09-17 22:53:42 -0600

    Day 9: lesson 5

---
+++ Día 9/modulo_math.py
@@ -0,0 +1,40 @@
+import math
+
+resultado_redondeo_abajo = math.floor(89.665)
+resultado_redonde_arriba = math.ceil(89.665)
+resultado_pi = math.pi
+resultado_logaritmo = math.log(25,5)
+resultado_tangente = math.tan(25,5)
+resultado_coseno = math.cos(25,5)
+
+print(resultado_pi)
+
+"""
+Práctica Módulo Math 1
+Obtén el logaritmo base 10 del número 25, y almacena el resultado en la variable resultado.
+
+Puedes utilizar el método math.log10()
+
+Puedes consultar el enlace anterior si quieres conocer más acerca del logaritmo decimal.
+"""
+
+import math
+
+resultado = math.log10(25)
+
+"""
+Práctica Módulo Math 2
+Obten la raíz cuadrada de pi con la constante math.pi y el método math.sqrt() . Almacena el resultado obtenido en la variable resultado.
+"""
+
+import math
+
+resultado = math.sqrt(math.pi)
+
+"""
+
+"""
+
+import math
+
+resultado = math.factorial(7)
