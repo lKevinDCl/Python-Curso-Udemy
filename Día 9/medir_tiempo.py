637c467fa2b1bd7b936496ae2f23b3e116e6ad81
lKevinDCl <kevindiego2608@gmail.com>
2025-09-17 22:37:54 -0600

    Day 9: lesson 4

--- Día 9/medir_tiempo.py
+++ Día 9/medir_tiempo.py
@@ -1,1 +1,71 @@
-import time
+import timeit
+
+"""
+def prueba_for(numero):
+    lista = []
+    for num in range(1, numero + 1):
+        lista.append(num)
+    return lista
+
+def prueba_while(numero):
+    lista = []
+    contador = 1
+    while contador <= numero:
+        lista.append(contador)
+        contador += 1
+    return lista
+
+inicio = time.time()
+prueba_for(1000000)
+final = time.time()
+print(final - inicio)
+
+inicio = time.time()
+prueba_while(1000000)
+final = time.time()
+print(final - inicio)
+"""
+
+def prueba_for(numero):
+    lista = []
+    for num in range(1, numero + 1):
+        lista.append(num)
+    return lista
+
+def prueba_while(numero):
+    lista = []
+    contador = 1
+    while contador <= numero:
+        lista.append(contador)
+        contador += 1
+    return lista
+
+declaracion = """
+prueba_for(10)
+"""
+mi_setup = """
+def prueba_for(numero):
+    lista = []
+    for num in range(1, numero + 1):
+        lista.append(num)
+    return lista
+"""
+
+duracion = timeit.timeit(declaracion, mi_setup, number=1000000)
+print(duracion)
+
+declaracion2 = """
+prueba_while()
+"""
+mi_setup2 = """
+def prueba_while(numero):
+    lista = []
+    contador = 1
+    while contador <= numero:
+        lista.append(contador)
+        contador += 1
+    return lista
+"""
+
+duracion2 = timeit.timeit(declaracion2, mi_setup2, number=1000000)
+print(declaracion2)
