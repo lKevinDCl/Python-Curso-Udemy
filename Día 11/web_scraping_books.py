7a9d4de4299002989d1eeab10ac85fdeb4cd1331
lKevinDCl <kevindiego2608@gmail.com>
2025-09-18 15:54:50 -0600

    Day 11: Lesson 4 and 5

--- Día 11/web_scraping_books.py
+++ Día 11/web_scraping_books.py
@@ -5,5 +5,11 @@ url_base = 'https://books.toscrape.com/catalogue/page-{}.html'
 
 print(url_base.format('15'))
 
-for n in range (1, 11):
+"""for n in range (1, 11):
     print(url_base.format(n))
+"""
+
+resultado = requests.post(url_base.format('1'))
+sopa = bs4.BeautifulSoup(resultado.text, 'lmxl')
+
+print(sopa.select('.product.pod'))
