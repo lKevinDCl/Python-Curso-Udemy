3e942dd93cb43fddf156318edaafcfddf334e21e
lKevinDCl <kevindiego2608@gmail.com>
2025-09-18 13:51:35 -0600

    Day 11: Lesson 1 and 2 and 3

--- Día 11/web_scraping.py
+++ Día 11/web_scraping.py
@@ -1,19 +1,39 @@
+from fileinput import close
+
 import bs4
 import requests
+from urllib3 import request
 
-resultado = requests.get('https://escueladirecta-blog.blogspot.com/')
+#resultado = requests.get('https://escueladirecta-blog.blogspot.com/')
+resultado =requests.get('https://www.escueladirecta.com/l/products')
 
 # print(resultado.text)
-
+"""
 sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
 
-print(sopa.select('title')[0].getText())
+print(sopa.select('title')[0].getText())"""
 
 """parrafo_especial = sopa.select('p')[3].getText()
 print(parrafo_especial)"""
 
-columna_lateral = sopa.select('.content p')
+"""columna_lateral = sopa.select('.content p')
 print(columna_lateral)
 
 for p in columna_lateral:
-    print(p.getText())
+    print(p.getText())"""
+
+sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+
+"""imagenes = sopa.select('img')
+for i in imagenes:
+    print(i)"""
+
+imagenes = sopa.select('.couser-box-image')[0]['src']
+print(imagenes)
+
+imagen_curso_1 = requests.get(imagenes)
+
+f= open('mi:imagen.jpg','wb')
+f.write(imagen_curso_1)
+f.close()
+
